bd23ffa367137431ca16ae4e71d996b193adf714
Du Hailong <1719812714@qq.com>
2017-06-28 19:03:20 +0800

    UILabel category

---
+++ OC_Project/Category/UILabel+hl.h
@@ -0,0 +1,43 @@
+//
+//  UILabel+hl.h
+//  OC_Project
+//
+//  Created by 杜海龙 on 17/6/28.
+//  Copyright © 2017年 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (hl)
+/**
+ *  字间距
+ */
+@property (nonatomic,assign)CGFloat characterSpace;
+
+/**
+ *  行间距
+ */
+@property (nonatomic,assign)CGFloat lineSpace;
+
+/**
+ *  关键字
+ */
+@property (nonatomic,copy)NSString *keywords;
+@property (nonatomic,strong)UIFont *keywordsFont;
+@property (nonatomic,strong)UIColor *keywordsColor;
+
+/**
+ *  下划线
+ */
+@property (nonatomic,copy)NSString *underlineStr;
+@property (nonatomic,strong)UIColor *underlineColor;
+
+/**
+ *  计算label宽高，必须调用
+ *
+ *  @param maxWidth 最大宽度
+ *
+ *  @return label的size
+ */
+- (CGSize)getLableSizeWithMaxWidth:(CGFloat)maxWidth;
+@end
