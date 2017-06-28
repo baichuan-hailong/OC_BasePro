a38d49f07f6dba053cb5d8e8253f359a3c3ef318
Du Hailong <1719812714@qq.com>
2017-06-28 18:34:53 +0800

    Add mac&&pch file

---
+++ OC_Project/Config/GloDefine.h
@@ -0,0 +1,13 @@
+//
+//  GloDefine.h
+//  OC_Project
+//
+//  Created by 杜海龙 on 17/6/28.
+//  Copyright © 2017年 yoyo. All rights reserved.
+//
+
+#ifndef GloDefine_h
+#define GloDefine_h
+
+
+#endif /* GloDefine_h */
