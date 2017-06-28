f065fe0f96b61fa01a4fb4739cf563a3400d08dc
Du Hailong <1719812714@qq.com>
2017-06-28 18:43:02 +0800

    update

--- OC_Project/ViewController.m
+++ OC_Project/ViewController.m
@@ -16,13 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 
